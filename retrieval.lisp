347473b18e5f22ff44bafd0f1e8f1dbda4e202a7
Olaf Merkert <olaf@m-merkert.de>
2011-09-07 22:21:11 +0200

    scrape imdb sites for title search, details and episodes

--- retrieval.lisp
+++ retrieval.lisp
@@ -5,24 +5,125 @@
 
 (in-package :md-retrieval)
 
-(defmacro define-grab (name params grab-url &rest expressions))
+(defun page-from-url (url)
+  (chtml:parse
+   (drakma:http-request url)
+   (chtml:make-lhtml-builder)))
+
+(defmacro define-grab (name params grab-url temporary-vars &body result)
+  `(defun ,name ,params
+     (let ((page (page-from-url
+                  (format nil ,grab-url ,@params))))
+       (let* ,temporary-vars
+         ,@result))))
+
+(defmacro! define-standard-print-object
+    (class &rest slots)
+  (let ((slots-flat (flatten slots)))
+    `(defmethod print-object ((,g!object ,class) ,g!stream)
+       (print-unreadable-object (,g!object ,g!stream :type t)
+         (with-slots ,slots-flat ,g!object
+           (format ,g!stream ,(format nil "~{~A~^ ~}"
+                                      (mapcar (lambda (x)
+                                                (if (listp x)
+                                                    "[~A]" "~A"))
+                                              slots))
+                   ,@slots-flat))))))
+
+(defclass name ()
+  ((name :accessor name
+         :initarg  :name
+         :initform "")
+   (imdb :accessor imdb
+         :initarg  :imdb
+         :initform "")))
+
+(define-standard-print-object name  name (imdb))
+
+(defclass title ()
+  ((name :accessor name
+         :initarg  :name
+         :initform "")
+   (imdb :accessor imdb
+         :initarg  :imdb
+         :initform "")
+   (year)
+   (original-name)))
+
+(define-standard-print-object title  name (imdb))
+
+(defclass episode ()
+  ((season :accessor season
+           :initarg  :season
+           :initform 1)
+   (episode :accessor episode
+            :initarg  :episode
+            :initform 1)
+   (name :accessor name
+         :initarg  :name
+         :initform "")
+   (title :accessor title
+          :initarg  :title
+          :initform nil)))
+
+(define-standard-print-object episode  season episode (name))
 
 (define-grab title-search (search-string)
     "http://www.imdb.com/find?s=tt&q=~A"
-    ())
+    ((nodes (css-select page
+                        (:div :id "main")
+                        (:table :style nil)
+                        (:tr)
+                        (child 2)
+                        (:a))))
+  (mapcar (lambda (node)
+            (make-instance 'title
+                           :name (tag-child node 0)
+                           :imdb (tag-attribute node :href)))
+          nodes))
 
-(defparameter *example-url*
-  (format nil "http://www.imdb.com/find?s=tt&q=~A"
-          "maxe"))
+(defun link->name (a)
+  (make-instance 'name :name (tag-text a)
+                 :imdb (tag-attribute a :href)))
 
-(defparameter *example-page*
-  (chtml:parse (drakma:http-request *example-url*)
-               (chtml:make-lhtml-builder)))
+(define-grab title-details (imdb)
+    "http://www.imdb.com~A"
+    ((title-header (css-select1 page (nil :itemprop "name")))
+     (name (tag-text title-header))
+     (year  (tag-text (css-select1 title-header (:a :href "^/year"))))
 
-(defun tryout-expression ()
-  (third
-   (css-select:css-select (:tr :td)
-          (second
-           (css-select:css-select
-            ((:div :id "main") (:table))
-            *example-page*)))))
+     (director(link->name (css-select1 page (nil :itemprop "director"))))
+
+     (actors (mapcar #'link->name (css-select page (nil :itemprop "actors"))))
+
+     (genres (mapcar #'tag-text (css-select page (nil :itemprop "genre")))))
+  
+  (list 'title 
+        :full t
+        :imdb imdb
+        :name name
+        :year year
+        :director director
+        :actors actors
+        :genres genres))
+
+
+(define-grab title-episodes (imdb)
+    "http://www.imdb.com~Aepisodes"
+    ((episode-nodes (css-select page
+                                (:div :class "season-filter-all")
+                                (:td)
+                                (:h3))))
+  (filter (lambda (x)
+            (let ((name (tag-text (css-select1 x :a)))
+                  (hits (or (not (stringp (tag-text x)))
+                            (nth-value 1
+                                       (cl-ppcre:scan-to-strings
+                                        "Season ([0-9]+), Episode ([0-9]+)"
+                                        (tag-text x))))))
+              (when hits
+                (make-instance 'episode
+                               :name name
+                               :season (parse-integer (aref hits 0))
+                               :episode (parse-integer (aref hits 1))))))
+          episode-nodes))
