24838a45d580b9d26de9641e9fd7986ee19bf909
Olaf Merkert <olaf@m-merkert.de>
2011-09-07 20:18:11 +0200

    weed out the mistakes and add more magic to css-selection

--- css-select.lisp
+++ css-select.lisp
@@ -7,7 +7,7 @@
            :css-selection
            :css-select
            :fun :next :previous
-           :tag :parent
+           :tag :parent :child
            :strip
            :string-contains))
 
@@ -17,7 +17,7 @@
 ;; lhtml interface
 (defun tag-p (tag)
   "Ein lhtml Tag ist eine Liste, die mit einem Symbol beginnt."
-  (and (listp tag)
+  (and (consp tag)
        (symbolp (first tag))))
 
 (defun tag-name (tag)
@@ -53,67 +53,79 @@
     ,@(mapcar #`(eq ,g!x ,a1) possibilities)))
 
 (defun special-selector-p (selector)
-  (in (car selector) 'fun 'parent 'next 'previous))
+  (in (car selector) 'fun 'parent 'next 'previous 'child))
 
 (defun selector->testfun (selector)
   (with-gensyms!
-   (let ((selector (mklist selector)))
-     (cond
-       ((eq (car selector) 'fun)                      ; special function evaluation
-        `(ilambda (tag parent)
-           ,@(cdr selector)))
-       ((eq (car selector) 'parent)
-        `(ilambda (tag parent)
-           parent))
-       ((eq (car selector) 'next)
-        `(ilambda (tag parent)
-           (tag-child parent
-                      (+ (position tag (tag-children parent)) 1))))
-       ((eq (car selector) 'previous)
-        `(ilambda (tag parent)
-           (tag-child parent
-                      (- (position tag (tag-children parent)) 1))))
-       
-       (t                               ; Standard Css selector
-        (ecase (length selector)
-          ;; Teste nur auf Tag Name
-          (1 `(ilambda (,g!tag ,g!parent) ,(tag-name-test (first selector) g!tag)))
-          ;; Teste auf Tag Name und Vorhandensein des Attributs
-          (2 `(ilambda (,g!tag ,g!parent)
-                (and ,(tag-name-test (first selector) g!tag)
-                     (tag-attribute ,g!tag ,(keyw (second selector))))))
-          ;; Test auf Tag Name und Inhalt des Attributs
-          (3 `(ilambda (,g!tag ,g!parent)
-                (and ,(tag-name-test (first selector) g!tag)
-                     (string-contains
-                      ,(third selector)
-                      (tag-attribute ,g!tag ,(keyw (second selector)))))))))))))
+    (let ((selector (mklist selector)))
+      (cond
+        ((eq (car selector) 'fun)       ; special function evaluation
+         `(ilambda (tag parent)
+            ,@(cdr selector)))
+        ((eq (car selector) 'parent)
+         `(ilambda (tag parent)
+            parent))
+        ((eq (car selector) 'next)
+         `(ilambda (tag parent)
+            (tag-child parent
+                       (+ (position tag (tag-children parent)) 1))))
+        ((eq (car selector) 'previous)
+         `(ilambda (tag parent)
+            (tag-child parent
+                       (- (position tag (tag-children parent)) 1))))
+        ((eq (car selector) 'child)
+         `(ilambda (tag parent)
+            (tag-child tag ,(second selector))))
+        (t                              ; Standard Css selector
+         (ecase (length selector)
+           ;; Teste nur auf Tag Name
+           (1 `(ilambda (,g!tag ,g!parent) ,(tag-name-test (first selector) g!tag)))
+           ;; Teste auf Tag Name und Vorhandensein des Attributs
+           (2 `(ilambda (,g!tag ,g!parent)
+                 (and ,(tag-name-test (first selector) g!tag)
+                      (tag-attribute ,g!tag ,(keyw (second selector))))))
+           ;; Test auf Tag Name und Inhalt des Attributs/ bzw Attribut nicht vorhanden!
+           (3 `(ilambda (,g!tag ,g!parent)
+                 (and ,(tag-name-test (first selector) g!tag)
+                      ,(if (eq (third selector) nil)
+                           ;; Attribut soll nicht vorhanden sein!
+                           `(not (tag-attribute ,g!tag ,(keyw (second selector))))
+                           ;; Teste Attribut auf regex
+                           `(string-contains
+                             ,(third selector)
+                             (tag-attribute ,g!tag ,(keyw (second selector))))))))))))))
 
 (defun css-selection (selector-stack special-stack tag-list &optional parent)
   "SELECTOR-STACK ist eine Liste von Funktionen, TAG-LIST eine Liste von lhtml Knoten."
+  #d
   ;; Noch weitere Selektoren
-  (cond ((null selector-stack)      ; no more selectors -- return data
-         (if parent
-             (list parent)
-             (copy-list tag-list)))
-        ((null tag-list))      ; no tags -- nothing to do
-        ((first special-stack)   ; filter through the special selector
-         (css-selection (rest selector-stack) (rest special-stack)
-                        (filter (lambda (x) (funcall (first selector-stack) x parent))
-                                tag-list)))
-        ((funcall (first selector-stack) (first tag-list) parent)
-         ;; ^^ the first item matches
-         (nconc
-          (css-selection (rest selector-stack) (rest special-stack)
-                         (tag-children (first tag-list))
-                         (first tag-list))
-          #1=(css-selection selector-stack special-stack (rest tag-list) parent)))
-        (t                              ; no match, go deeper
-         (nconc
-          (css-selection selector-stack special-stack
-                         (tag-children (first tag-list))
-                         (first tag-list))
-          #1#))))
+  (let* ((tag (first tag-list))
+         (tagp (tag-p tag)))
+    (cond ((null selector-stack)    ; no more selectors -- return data
+           (if parent
+               (list parent)
+               (copy-list tag-list)))
+          ((null tag-list) nil)      ; no tags -- nothing to do
+          ((first special-stack) ; filter through the special selector
+           (css-selection (rest selector-stack) (rest special-stack)
+                          (filter (lambda (x) (funcall (first selector-stack) x parent))
+                                  tag-list)))
+          ((and tagp
+                (funcall (first selector-stack) tag parent))
+           ;; ^^ the first item is actually a tag and matches
+           (nconc
+            (css-selection (rest selector-stack) (rest special-stack)
+                           (tag-children tag)
+                           tag)
+            #1=(css-selection selector-stack special-stack (rest tag-list) parent)))
+          (tagp            ; first item is a tag, doesn't match though
+           (nconc
+            (css-selection selector-stack special-stack
+                           (tag-children tag)
+                           tag)
+            #1#))
+          (t                            ; first item not a tag
+           #1#))))
 
 (defmacro css-select (selector &rest nodes)
   "Benutze eine Art von CSS-Selektoren, um einen lhtml-Baum zu durchforsten."
